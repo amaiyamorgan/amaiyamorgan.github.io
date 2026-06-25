3b18a14f2e47040dbfa2c944a3fd9feaff7d0559
amaiyamorgan <amaiyamorgan40@gmail.com>
2026-06-25 15:47:58 -0400

    Add school and fun fact variables to jsdemo.js

--- jsdemo.js
+++ jsdemo.js
@@ -1,1 +1,8 @@
 console.log("This is loaded from the external JS file");
+var name = "Amaiya";
+let school = "Cato Middle College High School";
+var Funfact = "I am my own beauty technician"
+
+console.log(`Name: ${name}`);
+console.log(`School: ${school}`);
+console.log(`Fun Fact: ${Funfact}`);
