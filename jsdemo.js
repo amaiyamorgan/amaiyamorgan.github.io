8b0b078641ecd843eca4e7a628471b14c54f77a6
amaiyamorgan <amaiyamorgan40@gmail.com>
2026-06-30 15:41:13 -0400

    Fix formatting of Funfact variable declaration

--- jsdemo.js
+++ jsdemo.js
@@ -1,7 +1,7 @@
 console.log("This is loaded from the external JS file");
 var name = "Amaiya";
 let school = "Cato Middle College High School";
-var Funfact = "I am my own beauty technician"
+var Funfact = "I am my own beauty technician";
 
 console.log(`Name: ${name}`);
 console.log(`School: ${school}`);
