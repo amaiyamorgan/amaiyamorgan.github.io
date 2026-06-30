9588ac4477bfb868e5b11d12cb04a7ca351d6edb
amaiyamorgan <amaiyamorgan40@gmail.com>
2026-06-30 15:52:33 -0400

    Display user information in HTML
    Add HTML elements to display name, school, and fun fact.

--- jsdemo.js
+++ jsdemo.js
@@ -6,3 +6,7 @@ var Funfact = "I am my own beauty technician";
 console.log(`Name: ${name}`);
 console.log(`School: ${school}`);
 console.log(`Fun Fact: ${Funfact}`);
+
+document.getElementById("name").innerHTML = "<b>Name: </b><i>" + name + "</i>";
+document.getElementById("school").innerHTML = "<b>School: </b><i>" + school + "</i>";
+document.getElementById("Funfact").innerHTML = "<b>Fun fact: </b><i>" + Funfact + "</i>";
